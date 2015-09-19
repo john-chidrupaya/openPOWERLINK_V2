70505e7223ce05681b9783bb661205b50dbaac9c
John <chidrupaya.sr@kalycito.com>
2015-09-19 15:17:49 +0530

    [TASK] Cleanup conf store restore implementation

--- stack/include/oplk/obdconf.h
+++ stack/include/oplk/obdconf.h
@@ -1,82 +1,50 @@
-/****************************************************************************
+/**
+********************************************************************************
+\file   oplk/obdconf.h
 
-  (c) SYSTEC electronic GmbH, D-08468 Heinsdorfergrund, Am Windrad 2
-      www.systec-electronic.com
+\brief  Target specific functions for OD store/restore
 
-  Project:      openPOWERLINK
+This file contains the target specific functions and definitions for OD store/restore.
 
-  Description:  target specific functions for OD store/restore
+*******************************************************************************/
 
-  License:
+/*------------------------------------------------------------------------------
+Copyright (c) 2015, SYSTEC electronic GmbH, D-08468 Heinsdorfergrund, Am Windrad 2
+Copyright (c) 2015, Kalycito Infotech Private Limited
+All rights reserved.
 
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions
-    are met:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holders nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
-    1. Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+------------------------------------------------------------------------------*/
 
-    2. Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-
-    3. Neither the name of SYSTEC electronic GmbH nor the names of its
-       contributors may be used to endorse or promote products derived
-       from this software without prior written permission. For written
-       permission, please contact info@systec-electronic.com.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-    POSSIBILITY OF SUCH DAMAGE.
-
-    Severability Clause:
-
-        If a provision of this License is or becomes illegal, invalid or
-        unenforceable in any jurisdiction, that shall not affect:
-        1. the validity or enforceability in that jurisdiction of any other
-           provision of this License; or
-        2. the validity or enforceability in other jurisdictions of that or
-           any other provision of this License.
-
-  -------------------------------------------------------------------------
-
-                $RCSfile$
-
-                $Author$
-
-                $Revision$  $Date$
-
-                $State$
-
-                Build Environment:
-                    GCC V3.4
-
-  -------------------------------------------------------------------------
-
-  Revision History:
-
-  2013/01/08 d.k.:   start of the implementation
-
-****************************************************************************/
-
-#ifndef _EPLTGTOBDARC_H_
-#define _EPLTGTOBDARC_H_
+#ifndef _INC_oplk_obdconf_H_
+#define _INC_oplk_obdconf_H_RC_H_
 
 
 //---------------------------------------------------------------------------
 // const defines
 //---------------------------------------------------------------------------
 
-// storage read access parameters for objects 0x1010 and 0x1011 (see EPSG 301)
+// Storage read access parameters for objects 0x1010 and 0x1011 (see EPSG 301)
 #define OBD_STORE_UNSUPPORTED   0x00000000L
 #define OBD_STORE_ON_COMMAND    0x00000001L
 #define OBD_STORE_AUTONOMOUSLY  0x00000002L
@@ -104,5 +72,5 @@ BOOL obdconf_isPartArchiveValid(tObdPart odPart_p);
 tOplkError obdconf_setBackupArchivePath(const char* pBackupPath_p);
 tOplkError obdconf_setPartSignature(tObdPart odPart_p, UINT32 signature_p);
 
-#endif  // #ifndef _EPLTGTOBDARC_H_
+#endif  // #ifndef _INC_oplk_obdconf_H_
 
